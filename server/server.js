4ebabececa163be3a3e449943f59cc932328fce1
Chuck Marlow <marlowchu@gmail.com>
2022-04-09 15:04:59 -0400

    API working

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-require('dotenv').config()
+// require('dotenv').config()
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
